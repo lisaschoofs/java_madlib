6e9e5e00aa17bd338a29ba939e7bc645a8612fbf
Lisa Schoofs <lisaschoofs@gmail.com>
2017-06-13 19:55:57 -0500

    initial class setup

---
+++ Madlibs.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+
+public class Madlibs {
+
+  public static void main(String[] args) {
+
+    Console console = System.console();
+
+    console.printf("hello!");
+    
+  }
+}
